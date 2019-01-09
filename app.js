c123c69db45d7f12f055a0547e9d7a13f4b46db5
adityarao97 <adi97rao@gmail.com>
2019-01-09 23:40:42 +0530

    major optimization work

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get("/gallery",function(req,res){
     res.render("gallery",{val: val});
 });
 
+
 app.get("/events",function(req,res){
     val="events";
     res.render("events",{val: val});
@@ -26,3 +27,7 @@ if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port);
+
+// app.listen(3000,function(){
+//     console.log("cosmos is running on port no 3000");
+// });
