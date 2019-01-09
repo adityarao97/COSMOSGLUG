5d747e09aeee7d3a1faf45a8ecdf5e0fa12e071e
adityarao97 <adi97rao@gmail.com>
2019-01-09 11:47:14 +0530

    live project started

---
+++ app.js
@@ -0,0 +1,26 @@
+var express=require("express");
+var app=express();
+var bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+
+var val;
+app.get("/",function(req,res){
+    val="main";  
+    res.render("main",{val: val});
+});
+
+app.get("/gallery",function(req,res){
+    val="gallery";
+    res.render("gallery",{val: val});
+});
+
+app.get("/events",function(req,res){
+    val="events";
+    res.render("events",{val: val});
+});
+
+app.listen(3000,function(){
+    console.log("cosmos is running on port no 3000");
+});
